832fc246103cddad2519c94aa6e35d560f10a31e
ac-gomes <acgoms88@gmail.com>
2022-02-08 21:47:04 -0300

    icon added

--- src/component/Icon.js
+++ src/component/Icon.js
@@ -1,11 +1,22 @@
 import Tools from "../helper/tools.js";
+import doc from "../data/documentation.js";
 
-const Icon = (label, className, color) => {
+const Icon = (label, className, color, link) => {
   let icon = Tools.createElement('span')
+  let functionDoc = Tools.createElement('a')
+
+  functionDoc.href = doc.push
+  functionDoc.target="_blank"
+  functionDoc.rel="noopener noreferrer"
+  Tools.elementContent(functionDoc, label)
+  icon.appendChild(functionDoc)
+
   icon.className = className
   Tools.insertStyle(icon, color,true)
-  Tools.elementContent(icon, label)
+  console.log(functionDoc)
   return icon
 }
+
+
 
 export default Icon;
---
+++ src/data/documentation.js
@@ -0,0 +1,6 @@
+const doc = {
+  push: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/push',
+  pop: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/pop',
+};
+
+export default doc;
